ba9e5a6b1bb2fbebefb748fb4838ba97b035148f
Freeze88 <andrevitorino1997@hotmail.com>
2019-04-10 16:40:29 +0100

    First version of 'the seed', sripts for camera movement and player movement, mechanics introduced such as gravity switch

---
+++ Assets/scripts/MovementController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    bool isGrounded = false;
+    public float speed = 100;
+    public float jump = 8.0f;
+    bool isFloating = false;
+
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer rot;
+    Vector3 moveVector;
+    Vector3 jumpForce;
+    Vector3 currentVelocity;
+
+    Vector3 gravity = new Vector3(0f, -9.81f, 0f);
+    
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rot = GetComponent<SpriteRenderer>();
+        rb.gravityScale = 0;
+    }
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        currentVelocity = moveVector;
+
+        rb.velocity = currentVelocity;
+
+        if (anim)
+        {
+            float hAxis = Input.GetAxis("Horizontal");
+
+            if (hAxis > 0.0f || hAxis < 0.0f)
+            {
+                anim.SetFloat("Speed", 1);
+            }
+            else
+            {
+                anim.SetFloat("Speed", 0);
+            }
+
+        }
+    }
+    private void Update()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+
+        if (isFloating)
+            isGrounded = Physics2D.Raycast(transform.position + new Vector3(0, GetComponent<CapsuleCollider2D>().bounds.extents.y, 0f), gravity.normalized, 0.5f, ~(1 << LayerMask.NameToLayer("Player")));
+        else
+            isGrounded = Physics2D.Raycast(transform.position - new Vector3(0, GetComponent<CapsuleCollider2D>().bounds.extents.y , 0f), gravity.normalized, 0.5f, ~(1 << LayerMask.NameToLayer("Player")));
+
+        if (!isGrounded)
+            jumpForce.y += 100 * gravity.y * Time.deltaTime;
+        else
+            jumpForce.y = gravity.y * Time.deltaTime;
+
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            jumpForce.y = isFloating ? -jump : jump;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            //rb.gravityScale = -rb.gravityScale;
+            gravity *= -1;
+            isFloating = !isFloating;
+            rot.flipY = !rot.flipY;
+        }
+
+        jumpForce = Vector3.Lerp(jumpForce, Vector3.zero, Time.deltaTime);
+        moveVector += jumpForce;
+
+        if (moveVector.x < 0.0f && transform.rotation.y == 0.0f)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+        else if (moveVector.x > 0.0f && transform.rotation.y < 0.0f)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(0.0f, -180.0f, 0.0f);
+        }
+
+    }
+}
+
---
+++ Assets/scripts/camera_move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_move : MonoBehaviour
+{
+    float Yaxis;
+    public GameObject Player;
+
+    private Vector3 offset;
+
+    public float limit = 150.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Yaxis = Input.GetAxis("Vertical");
+
+        /*if (Yaxis > 0.0f && transform.rotation.z == 0.0f)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(0.0f, 0.0f, 180.0f);
+        }
+        else if (Yaxis < 0.0f && transform.rotation.z > 0.0f)
+        {
+            transform.rotation = transform.rotation * Quaternion.Euler(0.0f, 0.0f, -180.0f);
+        }*/
+
+        if (Player.transform.position.x >= transform.position.x + limit)
+            transform.position = new Vector3 (Player.transform.position.x - limit, transform.position.y, transform.position.z);
+
+        else if (Player.transform.position.x <= transform.position.x - limit)
+            transform.position = new Vector3(Player.transform.position.x + limit, transform.position.y, transform.position.z);
+        
+
+    }
+}
