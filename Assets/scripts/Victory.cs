6c2c5bcc81be28d7d4072c81f047794536f8e710
some cunt who flunked C <afonso_rosa@hotmail.com>
2019-05-25 23:24:28 +0100

    workaround merge conflicts invisible bug fixed

---
+++ Assets/scripts/Victory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public Transform teleportTo;
+    public Transform Player;
+    [SerializeField] float Timer;
+    bool InsidePortal;
+
+    bool isIn
+    {
+        get
+        {
+            Collider2D collider = Physics2D.OverlapCircle(transform.position, 20.0f, LayerMask.GetMask("Player"));
+            return (collider != null);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (Timer <= 0)
+        {
+            Timer = 0;
+        }
+        else
+        {
+            Timer--;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        InsidePortal = isIn;
+
+        if (InsidePortal && Input.GetKeyDown(KeyCode.E))
+        {
+            Application.Quit();
+        }
+    }
+}
